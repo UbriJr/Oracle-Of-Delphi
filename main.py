a4db3601872e1f06b270a1a0ecd7632b65d33f4f
eddieubri <eddieubrijr@yahoo.com>
2021-02-13 01:55:44 -0500

    added instrutions on how to install openCV

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 # Documentation:
 # n/a
 
+# Install OpenCV in order for the imports to work properly,
+# open the terminal on your machine and run the command:
+# Mac / Linux users: pip3 install opencv-python
+# Windows users: pip install opencv-python
+# or hover over the red underlined import and select "install opencv"
+
+# OpenCV import:
+import cv2
+
 
 
 
