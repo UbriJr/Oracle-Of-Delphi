1e383542e66f3b46d6a269c1e76e2d9976476af7
eddieubri <eddieubrijr@yahoo.com>
2021-03-09 21:46:22 -0500

    attempted to fix toolbar on splash screen

--- main.py
+++ main.py
@@ -24,10 +24,7 @@ splash_root.overrideredirect(1)
 splash_root.overrideredirect(0)
 
 # Removes Option to Close out of Loading Screen
-splash_root.overrideredirect(True)
-splash_root.geometry("{0}x{1}+0+0".format(splash_root.winfo_screenwidth(),splash_root.winfo_screenheight()))
-splash_root.resizable(width=False, height=False)
-
+# splash_root.overrideredirect(True)
 
 # Adjust size & Centers Splash Screen
 splash_root_width = 640
